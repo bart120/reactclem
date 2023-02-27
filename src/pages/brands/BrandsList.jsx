c8752d17e463987b9a4713de90979e428a4c4bdd
Vincent Leclerc <vleclerc@inow.fr>
2023-02-27 16:03:03 +0100

    change state

--- src/pages/brands/BrandsList.jsx
+++ src/pages/brands/BrandsList.jsx
@@ -1,13 +1,5 @@
-import { Button, Card, CardContent, CardMedia, Grid, Paper, Typography, styled } from "@mui/material";
+import { Button, Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
 import { Component, useState } from "react";
-
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
 
 export default function BrandsList() {
     const [name, setNom] = useState("Bob");
@@ -18,31 +10,28 @@ export default function BrandsList() {
         setNom("Toto");
     }
 
-
-
     return (
         <div>
             <h1>Liste des marques par {name}</h1>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid xs={6}>
-                    <Item>
-                        <Card sx={{ maxWidth: 345 }}>
-                            <CardMedia
-                                sx={{ height: 140 }}
-                                image="/static/images/cards/contemplative-reptile.jpg"
-                                title="green iguana"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Lizard
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                                    species, ranging across all continents except Antarctica
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Item>
+                <Grid item xs={6}>
+
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            sx={{ height: 140 }}
+                            image="/static/images/cards/contemplative-reptile.jpg"
+                            title="green iguana"
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                Lizard
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Lizards are a widespread group of squamate reptiles, with over 6,000
+                                species, ranging across all continents except Antarctica
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </Grid>
 
             </Grid>
