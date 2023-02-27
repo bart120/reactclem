8d981f18c6f53f488c074830379a0dd65fdae15b
Vincent Leclerc <vleclerc@inow.fr>
2023-02-27 16:52:15 +0100

    state

--- src/pages/brands/BrandsList.jsx
+++ src/pages/brands/BrandsList.jsx
@@ -1,5 +1,5 @@
 import { Button, Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
-import { Component, useState } from "react";
+import { Component, useEffect, useState } from "react";
 
 export default function BrandsList() {
     const [name, setNom] = useState("Bob");
@@ -10,32 +10,39 @@ export default function BrandsList() {
         setNom("Toto");
     }
 
+    // = componentdidupdate
+    useEffect(() => {
+        //console.log("componentdidupdate");
+    });
+
+    // = componentdidmount
+    useEffect(() => {
+        //console.log("componentdidmount")
+        setBrands([{ name: 'renault', image: 'renault.jpg' }, { name: 'audi', image: 'audi.jpg' }]);
+    }, []);
+
     return (
         <div>
             <h1>Liste des marques par {name}</h1>
             <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={6}>
-
-                    <Card sx={{ maxWidth: 345 }}>
-                        <CardMedia
-                            sx={{ height: 140 }}
-                            image="/static/images/cards/contemplative-reptile.jpg"
-                            title="green iguana"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                Lizard
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Lizards are a widespread group of squamate reptiles, with over 6,000
-                                species, ranging across all continents except Antarctica
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-
+                {brands.map(brand => (
+                    <Grid item xs={6} key={brand.name}>
+                        <Card sx={{ maxWidth: 345 }}>
+                            <CardMedia
+                                sx={{ height: 140 }}
+                                image={`/images/${brand.image}`}
+                                title="green iguana"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {brand.name}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                ))}
             </Grid>
-            {/*<Button onClick={test}>Test</Button>*/}
+            <Button onClick={test}>Test</Button>
         </div>
     );
 }
@@ -49,11 +56,33 @@ class BrandsList2 extends Component {
         this.setState({ name: "Toto" });
     }
 
+    componentDidMount() {
+        //cht data
+        this.setState({ brands: [{ name: 'renault' }, { name: 'audi' }] });
+    }
+
     render() {
         return (
             <div>
                 <h1>Liste des marques par {this.state.name}</h1>
-                <Button onClick={this.test}>Test</Button>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    {this.state.brands.map(brand => (
+                        <Grid item xs={6} key={brand.name}>
+                            <Card sx={{ maxWidth: 345 }}>
+                                <CardMedia
+                                    sx={{ height: 140 }}
+                                    image="/static/images/cards/contemplative-reptile.jpg"
+                                    title="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {brand.name}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
             </div>
         );
     }
