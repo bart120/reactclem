4ab717ab248c293d045fd58d7e36623b4e2e32ba
Vincent Leclerc <vleclerc@inow.fr>
2023-02-28 10:27:42 +0100

    form

--- src/pages/authentication/Login.jsx
+++ src/pages/authentication/Login.jsx
@@ -1,11 +1,42 @@
-import { Component } from "react";
+import { Button, TextField } from "@mui/material";
+import React, { Component } from "react";
 
 class Login extends Component {
+    styleTextField = { width: "300px", margin: "5px" };
+    user = {};
+    state = {};
 
-    state = {}
+    constructor(props) {
+        super(props);
+        //this.handleSubmit = this.handleSubmit.bind(this);// remplace "=>"
+        //this.myMail = React.createRef(); //equiv getelementbyid
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("user:", this.user);
+    }
+
+    changeTextField = (e) => {
+        this.user[e.target.name] = e.target.value;
+        //console.log(this.user);
+    }
+
     render() {
         return (
-            <h1>Connexion</h1>
+            <div>
+                <h1>Connexion</h1>
+                <form noValidate onSubmit={this.handleSubmit}>
+                    <TextField type="email" label="Email" variant="outlined"
+                        ref={this.myMail} style={this.styleTextField} autoFocus
+                        onChange={this.changeTextField} name="login" />
+                    <br />
+                    <TextField type="password" label="Mot de passe" variant="outlined" style={this.styleTextField}
+                        onChange={this.changeTextField} name="password" />
+                    <br />
+                    <Button variant="contained" color="primary" type="submit">Se connecter</Button>
+                </form>
+            </div>
         );
     }
 }
